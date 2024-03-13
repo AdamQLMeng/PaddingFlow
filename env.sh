1e6aa00250645397b026f30136c513e24a0d3f52
Adam QL. Meng <114214603+AdamQLMeng@users.noreply.github.com>
2024-03-13 10:38:21 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ conda activate flows
 # Install dependencies
 pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 --index-url https://download.pytorch.org/whl/cu117
 pip install pandas pytorch_lightning matplotlib tqdm scikit-learn scipy tensorboardX torchdiffeq ninja imageio einops jrl==0.0.9 qpth -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install package/FrEIA-debug/
+pip install FrEIA==0.2
 pip install PyQT5
 sudo apt-get install libxcb-xinerama0
 conda install theano
